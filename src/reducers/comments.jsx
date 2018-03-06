747fe7b6bf006c5ecd1275ff4b9c778a37753dee
fikyair <shiming_xsm@163.com>
2018-03-06 15:27:55 +0800

    [增加]redux-react

---
+++ src/reducers/comments.jsx
@@ -0,0 +1,50 @@
+//action types
+const INIT_COMMENTS = 'INIT_COMMENTS'
+const ADD_COMMENTS = 'ADD_COMMENT'
+const DELETE_COMMENT = 'DELETE_COMMENT'
+
+//reducer
+export default function (state, action) {
+    if(!state) {
+        state = { comments: [] }
+    }
+
+    switch (action.type) {
+        case INIT_COMMENTS:
+            return { comments: action.comments }
+        case ADD_COMMENTS:
+            return {
+                comments: [...state.comments, action.comment]
+            }
+        case DELETE_COMMENT:
+            return{
+                comments: [
+                    ...state.comments.slice(0, action.commentIndex),
+                    ...state.comments.slice(action.commentIndex+1)
+                ]
+            }
+        default:
+            return state
+    }
+}
+
+//action creators
+
+export const initComments = (comments) =>{
+    return{
+        type: INIT_COMMENTS,comments
+    }
+}
+
+export const addComment = (comment) =>{
+    return{
+        type: ADD_COMMENTS,comment
+    }
+}
+export const deleteComment = (commentIndex) =>{
+    return{
+        type: DELETE_COMMENT,commentIndex
+    }
+}
+
+dispatch(initComments(comments))
